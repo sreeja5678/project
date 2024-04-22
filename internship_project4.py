d485e5c7de9c957c06574dbdefbc8640c8a6762c
sreeja5678 <165043077+sreeja5678@users.noreply.github.com>
2024-04-22 09:31:39 +0530

    Update internship_project4.py

--- internship_project4.py
+++ internship_project4.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(length):
-    #Generate a random password of the given length."""
+    #Generate a random password of the given length.
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for _ in range(length))
 
